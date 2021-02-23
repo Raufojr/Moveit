fa809498afe5942de598551a1c2ff24411f529af
Raufo Jr <raufo.rastrero@hotmail.com>
2021-02-22 21:29:38 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { ExperienceBAr } from './components/ExperienceBar';
+import './styles/global.css';
+
+
+function App() {
+  return (
+
+    <div className="container">
+      <ExperienceBAr />
+
+    </div>
+  );
+}
+
+export default App;
